764165ee452b364e98c91946cf9d03d7e4e5b28c
Jorge Villeda <jvilleda.dev@outlook.com>
2026-05-17 22:35:14 -0400

    feat(backend): initial Spring Boot project setup

---
+++ backend/src/test/java/com/jvilledaapps/gig_manager/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvilledaapps.gig_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
